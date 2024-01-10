13ac5f646a9c9caa972d11d2c5606b2f1ba0862d
Jon Green <jongreen1996@gmail.com>
2024-01-10 14:23:45 +0000

    Fixed hydration error in themeToggle

--- src/components/ui/themeToggle.jsx
+++ src/components/ui/themeToggle.jsx
@@ -3,9 +3,19 @@
 import { Button } from '@/components/ui/button';
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 export default function ThemeToggleButton({ className }) {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  // When the component is mounted, we'll set the mounted state to true
+  useEffect(() => setMounted(true), []);
+
+  // If the component is not mounted, don't render anything
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <Button

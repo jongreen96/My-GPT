f9418463e9d8f7b7fad13ab28510a60a20ef19a0
Jon Green <jongreen1996@gmail.com>
2024-01-11 15:31:31 +0000

    Added loading state to chat input and Autoscrolled chat

--- src/components/ui/chat.jsx
+++ src/components/ui/chat.jsx
@@ -7,28 +7,49 @@ import {
 } from '@/components/ui/messageBubble';
 import { useChat } from 'ai/react';
 import { usePathname, useRouter } from 'next/navigation';
+import { useEffect, useRef } from 'react';
 
 export default function Chat({ initialMessages, userId, id }) {
   const router = useRouter();
   const pathname = usePathname();
-  const { messages, input, handleInputChange, handleSubmit } = useChat({
-    initialMessages,
-    body: {
-      id,
-      userId,
-      newChat: pathname === '/chat',
-    },
-    onFinish: () => {
-      if (pathname !== `/chat/${id}`) {
-        router.push(`/chat/${id}`);
-        router.refresh();
-      }
-    },
-  });
+
+  const inputRef = useRef(null);
+  const scrollRef = useRef(null);
+
+  const { messages, input, handleInputChange, handleSubmit, error, isLoading } =
+    useChat({
+      initialMessages,
+      body: {
+        id,
+        userId,
+        newChat: pathname === '/chat',
+      },
+      onFinish: () => {
+        if (pathname !== `/chat/${id}`) {
+          router.push(`/chat/${id}`);
+          router.refresh();
+        }
+      },
+    });
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+    }
+  }, [messages]);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
 
   return (
     <>
-      <section className='no-scrollbar flex w-full flex-grow flex-col gap-2 p-2'>
+      <section
+        className='no-scrollbar flex w-full flex-grow flex-col gap-2 overflow-y-auto p-2'
+        ref={scrollRef}
+      >
         {messages.map((message) =>
           message.role === 'user' ? (
             <UserChatBubble message={message} key={message.id} />
@@ -36,11 +57,22 @@ export default function Chat({ initialMessages, userId, id }) {
             <AssistantChatBubble message={message} key={message.id} />
           ),
         )}
+        {error && (
+          <AssistantChatBubble
+            message={{
+              role: 'assistant',
+              content:
+                'Sorry, something went wrong. Please refresh the page and try again.',
+            }}
+          />
+        )}
       </section>
       <ChatInput
         input={input}
         handleInputChange={handleInputChange}
         handleSubmit={handleSubmit}
+        inputRef={inputRef}
+        isLoading={isLoading}
       />
     </>
   );
--- src/components/ui/chatInput.jsx
+++ src/components/ui/chatInput.jsx
@@ -1,10 +1,16 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
 import { SendHorizonal } from 'lucide-react';
 import TextAreaAuto from 'react-textarea-autosize';
+import LoadingButton from './loadingButton';
 
-export default function ChatInput({ input, handleInputChange, handleSubmit }) {
+export default function ChatInput({
+  input,
+  handleInputChange,
+  handleSubmit,
+  inputRef,
+  isLoading,
+}) {
   return (
     <form
       onSubmit={handleSubmit}
@@ -13,7 +19,9 @@ export default function ChatInput({ input, handleInputChange, handleSubmit }) {
       <TextAreaAuto
         autoFocus
         maxRows={15}
+        ref={inputRef}
         value={input}
+        placeholder='Say something...'
         onChange={handleInputChange}
         onKeyDown={(e) => {
           if (e.key === 'Enter' && !e.shiftKey) {
@@ -21,11 +29,17 @@ export default function ChatInput({ input, handleInputChange, handleSubmit }) {
             handleSubmit(e);
           }
         }}
+        disabled={isLoading}
         className='flex w-full resize-none self-end rounded-md border border-input bg-background px-3 py-2 shadow focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50'
       />
-      <Button size='icon' type='submit' className='self-end'>
+      <LoadingButton
+        loading={isLoading}
+        size='icon'
+        type='submit'
+        className='self-end text-white'
+      >
         <SendHorizonal />
-      </Button>
+      </LoadingButton>
     </form>
   );
 }

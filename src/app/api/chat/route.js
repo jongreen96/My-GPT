68376a6e6422e9abf12a183b6cae480746d54b64
Jon Green <jongreen1996@gmail.com>
2024-01-14 23:21:58 +0000

    Fix file size issue on build

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -2,7 +2,7 @@ import { streamConversation } from '@/lib/openAI';
 import { StreamingTextResponse } from 'ai';
 
 export const runtime = 'edge';
-
+// FIXME: See why this is 1.5MB on build
 export async function POST(req) {
   try {
     const stream = await streamConversation(req);

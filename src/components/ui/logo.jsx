ce0fec779b42060d12eadec4e33cfeb99aab51fd
Jon Green <jongreen1996@gmail.com>
2024-01-15 22:53:47 +0000

    Moved logo to public for page optimization

--- src/components/ui/logo.jsx
+++ src/components/ui/logo.jsx
@@ -1,21 +1,19 @@
 'use client';
 
-import darkLogoSVG from '@/app/assets/logo-dark.svg';
-import lightLogoSVG from '@/app/assets/logo-light.svg';
 import Image from 'next/image';
 
 export default function Logo() {
   return (
     <>
       <Image
-        src={lightLogoSVG}
+        src='/logo-light.svg'
         alt='my-gpt logo'
         width={38}
         height={38}
         className='dark:hidden'
       />
       <Image
-        src={darkLogoSVG}
+        src='/logo-dark.svg'
         alt='my-gpt logo'
         width={38}
         height={38}

863fb55e60d8dfc1fe2044d85e24bfb83066b7e7
Jon Green <jongreen1996@gmail.com>
2024-01-20 17:23:22 +0000

    Update styles for markdown code block

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -8,7 +8,7 @@ export function cn(...inputs) {
 export const markdownComponents = {
   pre: ({ node, ...props }) => (
     <>
-      <div className='flex w-full justify-between rounded-t bg-[#1a1a1a] p-1 pl-2 text-white'>
+      <div className='flex w-full justify-between rounded-t-lg border-b-2 border-[#333] bg-[#1a1a1a] p-1 px-2 text-white'>
         <p className='capitalize'>
           {node.children[0].properties['className']
             ? node.children[0].properties.className[0].slice(9)
@@ -18,12 +18,33 @@ export const markdownComponents = {
       </div>
       <pre
         {...props}
-        className='horizontal-scroll rounded-b bg-black p-2 text-white'
+        className='horizontal-scroll rounded-b-lg bg-black p-2 text-white'
       />
     </>
   ),
   ol: ({ node, ...props }) => (
     <ol {...props} className='flex list-decimal flex-col gap-6 pl-6' />
   ),
+  ul: ({ node, ...props }) => (
+    <ul {...props} className='flex list-disc flex-col gap-6 pl-6' />
+  ),
   li: ({ node, ...props }) => <li {...props} className='whitespace-normal' />,
+  h1: ({ node, ...props }) => <h1 {...props} className='text-4xl font-bold' />,
+  h2: ({ node, ...props }) => <h2 {...props} className='text-3xl font-bold' />,
+  h3: ({ node, ...props }) => <h3 {...props} className='text-2xl font-bold' />,
+  code: ({ node, ...props }) => (
+    <code
+      {...props}
+      className='inline-block rounded bg-black px-2 text-white'
+    />
+  ),
+  blockquote: ({ node, ...props }) => (
+    <blockquote
+      {...props}
+      className='my-2 border-l-2 border-brand pl-2 italic text-gray-500'
+    />
+  ),
+  a: ({ node, ...props }) => (
+    <a {...props} className='text-primary hover:underline' />
+  ),
 };

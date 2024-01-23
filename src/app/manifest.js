940a544234e5f0a49769520b4f43437c21c20af5
Jon Green <jongreen1996@gmail.com>
2024-01-23 14:44:38 +0000

    Swapped manifest.json for next function

---
+++ src/app/manifest.js
@@ -0,0 +1,8 @@
+export default function manifest() {
+  return {
+    name: 'My-GPT',
+    short_name: 'My-GPT',
+    description: 'OpenAI Wrapper to pay as you go',
+    display: 'standalone',
+  };
+}

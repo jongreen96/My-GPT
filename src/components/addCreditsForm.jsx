51a6b7d5d8f6ecd8b67e69699bf532edd0ccaf00
Jon Green <jongreen1996@gmail.com>
2024-02-25 18:06:58 +0000

    Add AfterPaymentMessage component to display payment status

--- src/components/addCreditsForm.jsx
+++ src/components/addCreditsForm.jsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { loadStripe } from '@stripe/stripe-js';
+import { AlertCircle, CheckCircleIcon } from 'lucide-react';
+import { useSearchParams } from 'next/navigation';
 import { Button } from './ui/button';
 import { Input } from './ui/input';
 import { RadioGroup } from './ui/radio-group';
@@ -12,9 +14,13 @@ const stripePromise = loadStripe(
 export default function AddCreditsForm({ credits }) {
   return (
     <div className='flex flex-col gap-6'>
-      <div className='flex items-baseline gap-2'>
-        <p className='text-2xl font-semibold'>{credits.toLocaleString()}</p>
-        <p>Credits</p>
+      <div className='flex justify-between'>
+        <div className='flex items-baseline gap-2'>
+          <p className='text-2xl font-semibold'>{credits.toLocaleString()}</p>
+          <p>Credits</p>
+        </div>
+
+        <AfterPaymentMessage />
       </div>
 
       <p className='text-lg font-semibold'>Add credits </p>
@@ -79,4 +85,26 @@ export default function AddCreditsForm({ credits }) {
       </form>
     </div>
   );
+}
+
+function AfterPaymentMessage() {
+  const params = useSearchParams();
+
+  if (params.get('success')) {
+    return (
+      <div className='flex items-center gap-2 text-green-500'>
+        <CheckCircleIcon size={24} />
+        <p>Payment Completed</p>
+      </div>
+    );
+  }
+
+  if (params.get('canceled')) {
+    return (
+      <div className='flex items-center gap-2 text-destructive'>
+        <AlertCircle size={24} />
+        <p>Payment Failed</p>
+      </div>
+    );
+  }
 }

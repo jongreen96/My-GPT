42cd71f7d384a35a44b73dd65b2ad10d94a61ddf
Jon Green <jongreen1996@gmail.com>
2024-03-08 16:41:17 +0000

    Improved chat images

--- src/components/chatImages.jsx
+++ src/components/chatImages.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
+import { Dialog, DialogContent, DialogTrigger } from './ui/dialog';
+
 export default function ChatImages({ images }) {
   if (!images || images.length === 0) return null;
 
@@ -5,13 +8,18 @@ export default function ChatImages({ images }) {
     <div className='ml-auto flex w-full max-w-[90%] flex-wrap justify-end gap-2 rounded-lg'>
       {images.map((image) => {
         return (
-          // eslint-disable-next-line @next/next/no-img-element
-          <img
-            key={image}
-            src={image}
-            alt='user image'
-            className='max-h-625 h-min w-full self-center rounded-lg sm:w-[373px]'
-          />
+          <Dialog key={image}>
+            <DialogTrigger asChild>
+              <img
+                src={image}
+                alt='user image'
+                className='w-full self-center rounded-lg sm:w-[186px]'
+              />
+            </DialogTrigger>
+            <DialogContent className='w-fit max-w-[95dvh] overflow-hidden rounded-lg border-0 p-0'>
+              <img src={image} alt='user image' className='' />
+            </DialogContent>
+          </Dialog>
         );
       })}
     </div>

792717c2b77ea2cd57779042424c8ae1314bb261
Jon Green <jongreen1996@gmail.com>
2024-03-19 14:23:53 +0000

    Update chat settings ready for other models

--- src/components/chatSettings.jsx
+++ src/components/chatSettings.jsx
@@ -16,7 +16,7 @@ import {
 } from '@/components/ui/select';
 import { IsDesktop } from '@/lib/hooks';
 import { openAIModels } from '@/lib/openAI';
-import { CogIcon, Settings2 } from 'lucide-react';
+import { Settings2 } from 'lucide-react';
 import {
   Drawer,
   DrawerContent,
@@ -27,7 +27,6 @@ import {
 import { Input } from './ui/input';
 import { Slider } from './ui/slider';
 import { Switch } from './ui/switch';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs';
 
 export default function ChatSettingsPopover({
   settings,
@@ -83,203 +82,181 @@ function innerContent(settings, setSettings) {
   const models = Object.keys(openAIModels);
 
   return (
-    <Tabs defaultValue='chat' className='w-full'>
-      <TabsList className='w-full'>
-        <TabsTrigger value='chat' className='w-full'>
-          Chat
-        </TabsTrigger>
-
-        <TabsTrigger value='image' className='w-full'>
-          Image
-        </TabsTrigger>
-
-        <TabsTrigger value='audio' className='w-full'>
-          Audio
-        </TabsTrigger>
-      </TabsList>
-
-      <TabsContent value='chat'>
-        <div className='flex flex-col gap-2'>
-          <div className='flex items-center justify-between gap-2'>
-            <Label htmlFor='model' className='w-40'>
-              Model:{' '}
-            </Label>
-            <Select
-              value={settings.model}
-              onValueChange={(value) =>
-                setSettings({ ...settings, model: value })
-              }
-            >
-              <SelectTrigger>
-                <SelectValue placeholder='Select a model'></SelectValue>
-              </SelectTrigger>
-              <SelectContent>
-                {models
-                  .filter(
-                    (model) =>
-                      openAIModels[model].type === 'chat' ||
-                      openAIModels[model].type === 'vision',
-                  )
-                  .map((model) => (
-                    <SelectItem key={model} value={model}>
-                      {model}
-                    </SelectItem>
-                  ))}
-              </SelectContent>
-            </Select>
-          </div>
-
-          <div className='flex items-center justify-between gap-2'>
-            <Label htmlFor='systemMessage' className='w-40'>
-              System message:{' '}
-            </Label>
-            <Input
-              id='systemMessage'
-              value={settings.system_message}
-              placeholder='Type system message here...'
-              onChange={(e) =>
-                setSettings({ ...settings, system_message: e.target.value })
-              }
-            />
-          </div>
-
-          <div className='flex items-center justify-between gap-2'>
-            <Label htmlFor='maxTokens' className='w-40'>
-              Max tokens:
-            </Label>
-            <Input
-              id='maxTokens'
-              type='number'
-              min={0}
-              value={
-                Number(settings.max_tokens) === 0 ? '' : settings.max_tokens
-              }
-              placeholder='unlimited'
-              onChange={(e) => {
-                setSettings({ ...settings, max_tokens: e.target.value });
-              }}
-            />
-          </div>
-
-          <div className='flex items-center justify-between gap-2'>
-            <Label htmlFor='temperature' className='w-40'>
-              Temperature:{' '}
-            </Label>
-            {settings.temperature}
-            <Slider
-              id='temperature'
-              min={0}
-              max={2}
-              defaultValue={[settings.temperature]}
-              step={0.1}
-              onValueChange={(value) =>
-                setSettings({ ...settings, temperature: value[0] })
-              }
-            />
-          </div>
-
-          <div className='flex items-center justify-between gap-2'>
-            <Label htmlFor='frequencyPenalty' className='w-40'>
-              Frequency penalty:{' '}
-            </Label>
-            {settings.frequency_penalty}
-            <Slider
-              id='frequencyPenalty'
-              min={-2}
-              max={2}
-              defaultValue={[settings.frequency_penalty]}
-              step={0.1}
-              onValueChange={(value) =>
-                setSettings({ ...settings, frequency_penalty: value[0] })
-              }
-            />
-          </div>
-
-          <div className='flex items-center justify-between gap-2'>
-            <Label htmlFor='presencePenalty' className='w-40'>
-              Presence penalty:{' '}
-            </Label>
-            {settings.presence_penalty}
-            <Slider
-              id='presencePenalty'
-              min={-2}
-              max={2}
-              defaultValue={[settings.presence_penalty]}
-              step={0.1}
-              onValueChange={(value) =>
-                setSettings({ ...settings, presence_penalty: value[0] })
-              }
-            />
-          </div>
-
-          <div className='flex items-center justify-between gap-2'>
-            <Label htmlFor='topP' className='w-40'>
-              Top P:{' '}
-            </Label>
-            {settings.top_p}
-            <Slider
-              id='topP'
-              min={0}
-              max={1}
-              defaultValue={[settings.top_p]}
-              step={0.01}
-              onValueChange={(value) =>
-                setSettings({ ...settings, top_p: value[0] })
-              }
-            />
-          </div>
-
-          {openAIModels[settings.model].type === 'chat' && (
-            <div className='flex items-center justify-between gap-2'>
-              <Label htmlFor='responseFormat' className='w-40'>
-                JSON response:{' '}
-              </Label>
-              <Switch
-                id='responseFormat'
-                checked={settings.response_format}
-                onCheckedChange={() =>
-                  setSettings({
-                    ...settings,
-                    response_format: !settings.response_format,
-                  })
-                }
-              />
-            </div>
-          )}
-
-          {openAIModels[settings.model].type === 'vision' && (
-            <div className='flex items-center justify-between gap-2'>
-              <Label htmlFor='highResVision' className='w-40'>
-                High res vision:{' '}
-              </Label>
-              <Switch
-                id='highResVision'
-                checked={settings.high_res_vision}
-                onCheckedChange={() =>
-                  setSettings({
-                    ...settings,
-                    high_res_vision: !settings.high_res_vision,
-                  })
-                }
-              />
-            </div>
-          )}
+    <div defaultValue='chat' className='w-full'>
+      <div className='flex flex-col gap-2'>
+        <div className='flex items-center justify-between gap-2'>
+          <Label htmlFor='model' className='w-40'>
+            Model:
+          </Label>
+          <Select
+            value={settings.model}
+            onValueChange={(value) =>
+              setSettings({ ...settings, model: value })
+            }
+          >
+            <SelectTrigger>
+              <SelectValue placeholder='Select a model'></SelectValue>
+            </SelectTrigger>
+            <SelectContent>
+              {models
+                .filter(
+                  (model) =>
+                    openAIModels[model].type === 'chat' ||
+                    openAIModels[model].type === 'vision',
+                )
+                .map((model) => (
+                  <SelectItem key={model} value={model}>
+                    {model}
+                  </SelectItem>
+                ))}
+            </SelectContent>
+          </Select>
         </div>
-      </TabsContent>
 
-      <TabsContent value='image'>
-        <div className='flex items-center justify-center gap-2 pt-2'>
-          <CogIcon className='h-6 w-6 animate-spin text-muted-foreground' />
-          <p>Coming soon...</p>
+        <div className='flex items-center justify-between gap-2'>
+          <Label htmlFor='systemMessage' className='w-40'>
+            System message:
+          </Label>
+          <Input
+            id='systemMessage'
+            value={settings.system_message}
+            placeholder='Type system message here...'
+            onChange={(e) =>
+              setSettings({ ...settings, system_message: e.target.value })
+            }
+          />
         </div>
-      </TabsContent>
 
-      <TabsContent value='audio'>
-        <div className='flex items-center justify-center gap-2 pt-2'>
-          <CogIcon className='h-6 w-6 animate-spin text-muted-foreground' />
-          <p>Coming soon...</p>
+        <div className='flex items-center justify-between gap-2'>
+          <Label htmlFor='maxTokens' className='w-40'>
+            Max tokens:
+          </Label>
+          <Input
+            id='maxTokens'
+            type='number'
+            min={0}
+            value={Number(settings.max_tokens) === 0 ? '' : settings.max_tokens}
+            placeholder='unlimited'
+            onChange={(e) => {
+              setSettings({ ...settings, max_tokens: e.target.value });
+            }}
+          />
         </div>
-      </TabsContent>
-    </Tabs>
+
+        <div className='flex items-center justify-between gap-2'>
+          <Label htmlFor='temperature' className='w-40'>
+            Temperature:
+          </Label>
+          {settings.temperature}
+          <Slider
+            id='temperature'
+            min={0}
+            max={2}
+            defaultValue={[settings.temperature]}
+            step={0.1}
+            onValueChange={(value) =>
+              setSettings({ ...settings, temperature: value[0] })
+            }
+          />
+        </div>
+
+        <div className='flex items-center justify-between gap-2'>
+          <Label htmlFor='frequencyPenalty' className='w-40'>
+            Frequency penalty:
+          </Label>
+          {settings.frequency_penalty}
+          <Slider
+            id='frequencyPenalty'
+            min={-2}
+            max={2}
+            defaultValue={[settings.frequency_penalty]}
+            step={0.1}
+            onValueChange={(value) =>
+              setSettings({ ...settings, frequency_penalty: value[0] })
+            }
+          />
+        </div>
+
+        <div className='flex items-center justify-between gap-2'>
+          <Label htmlFor='presencePenalty' className='w-40'>
+            Presence penalty:
+          </Label>
+          {settings.presence_penalty}
+          <Slider
+            id='presencePenalty'
+            min={-2}
+            max={2}
+            defaultValue={[settings.presence_penalty]}
+            step={0.1}
+            onValueChange={(value) =>
+              setSettings({ ...settings, presence_penalty: value[0] })
+            }
+          />
+        </div>
+
+        <div className='flex items-center justify-between gap-2'>
+          <Label htmlFor='topP' className='w-40'>
+            Top P:
+          </Label>
+          {settings.top_p}
+          <Slider
+            id='topP'
+            min={0}
+            max={1}
+            defaultValue={[settings.top_p]}
+            step={0.01}
+            onValueChange={(value) =>
+              setSettings({ ...settings, top_p: value[0] })
+            }
+          />
+        </div>
+
+        {modelSpecificOptions(settings, setSettings)}
+      </div>
+    </div>
   );
+}
+
+function modelSpecificOptions(settings, setSettings) {
+  const modelType = openAIModels[settings.model].type;
+
+  if (modelType === 'chat')
+    return (
+      <div className='flex items-center justify-between gap-2'>
+        <Label htmlFor='responseFormat' className='w-40'>
+          JSON response:
+        </Label>
+        <Switch
+          id='responseFormat'
+          checked={settings.response_format}
+          onCheckedChange={() =>
+            setSettings({
+              ...settings,
+              response_format: !settings.response_format,
+            })
+          }
+        />
+      </div>
+    );
+
+  if (modelType === 'vision')
+    return (
+      <div className='flex items-center justify-between gap-2'>
+        <Label htmlFor='highResVision' className='w-40'>
+          High res vision:
+        </Label>
+        <Switch
+          id='highResVision'
+          checked={settings.high_res_vision}
+          onCheckedChange={() =>
+            setSettings({
+              ...settings,
+              high_res_vision: !settings.high_res_vision,
+            })
+          }
+        />
+      </div>
+    );
+
+  return null;
 }

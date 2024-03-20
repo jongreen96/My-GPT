9d01925834a02038ea31f9941e648e07a9ca1fe4
Jon Green <jongreen1996@gmail.com>
2024-03-20 15:08:44 +0000

    Add descriptions to settings

--- src/components/defaultSettingsForm.jsx
+++ src/components/defaultSettingsForm.jsx
@@ -52,10 +52,13 @@ export default function DefaultSettingsForm({ user }) {
         <TabsContent value='chat'>
           <form
             action={updateDefaultChatSettingsAction}
-            className='flex flex-col gap-2'
+            className='flex flex-col gap-4'
           >
-            <div>
+            <div className='space-y-1'>
               <Label htmlFor='default_model'>Default Model</Label>
+
+              <p className='text-xs text-brand'>ID of the model to use.</p>
+
               <Select name='model' defaultValue={settings.model}>
                 <SelectTrigger>
                   <SelectValue placeholder={settings.model}></SelectValue>
@@ -75,8 +78,15 @@ export default function DefaultSettingsForm({ user }) {
               </Select>
             </div>
 
-            <div>
+            <div className='space-y-1'>
               <Label htmlFor='system_message'>System message:</Label>
+
+              <p className='text-xs text-brand'>
+                The system message is sent to the model before the user&apos;s
+                message. It can be used to provide context or instructions to
+                the model.
+              </p>
+
               <Input
                 id='system_message'
                 name='system_message'
@@ -84,8 +94,15 @@ export default function DefaultSettingsForm({ user }) {
               />
             </div>
 
-            <div>
+            <div className='space-y-1'>
               <Label htmlFor='max_tokens'>Max tokens:</Label>
+
+              <p className='text-xs text-brand'>
+                The maximum number of tokens that can be generated in the chat
+                completion. The total length of input tokens and generated
+                tokens is limited by the model&apos;s context length.
+              </p>
+
               <Input
                 id='max_tokens'
                 name='max_tokens'
@@ -98,11 +115,20 @@ export default function DefaultSettingsForm({ user }) {
               />
             </div>
 
-            <div>
+            <div className='space-y-1'>
               <div className='flex items-center justify-between'>
                 <Label htmlFor='temperature'>Temperature: </Label>
                 {settings.temperature}
               </div>
+
+              <p className='text-xs text-brand'>
+                What sampling temperature to use, between 0 and 2. Higher values
+                like 0.8 will make the output more random, while lower values
+                like 0.2 will make it more focused and deterministic. We
+                generally recommend altering this or top_p but not both.
+                (Default: 1)
+              </p>
+
               <Slider
                 id='temperature'
                 name='temperature'
@@ -116,11 +142,21 @@ export default function DefaultSettingsForm({ user }) {
               />
             </div>
 
-            <div>
+            <div className='space-y-1'>
               <div className='flex items-center justify-between'>
                 <Label htmlFor='top_p'>Top P: </Label>
                 {settings.top_p}
               </div>
+
+              <p className='text-xs text-brand'>
+                An alternative to sampling with temperature, called nucleus
+                sampling, where the model considers the results of the tokens
+                with top_p probability mass. So 0.1 means only the tokens
+                comprising the top 10% probability mass are considered. We
+                generally recommend altering this or temperature but not both.
+                (Default: 1)
+              </p>
+
               <Slider
                 id='top_p'
                 name='top_p'
@@ -134,11 +170,18 @@ export default function DefaultSettingsForm({ user }) {
               />
             </div>
 
-            <div>
+            <div className='space-y-1'>
               <div className='flex items-center justify-between'>
                 <Label htmlFor='presence_penalty'>Presence penalty: </Label>
                 {settings.presence_penalty}
               </div>
+
+              <p className='text-xs text-brand'>
+                Number between -2.0 and 2.0. Positive values penalize new tokens
+                based on whether they appear in the text so far, increasing the
+                model&apos;s likelihood to talk about new topics. (Default: 0)
+              </p>
+
               <Slider
                 id='presence_penalty'
                 name='presence_penalty'
@@ -152,11 +195,19 @@ export default function DefaultSettingsForm({ user }) {
               />
             </div>
 
-            <div>
+            <div className='space-y-1'>
               <div className='flex items-center justify-between'>
                 <Label htmlFor='frequency_penalty'>Frequency penalty: </Label>
                 {settings.frequency_penalty}
               </div>
+
+              <p className='text-xs text-brand'>
+                Number between -2.0 and 2.0. Positive values penalize new tokens
+                based on their existing frequency in the text so far, decreasing
+                the model&apos;s likelihood to repeat the same line verbatim.
+                (Default: 0)
+              </p>
+
               <Slider
                 id='frequency_penalty'
                 name='frequency_penalty'
@@ -170,19 +221,34 @@ export default function DefaultSettingsForm({ user }) {
               />
             </div>
 
-            <div className='flex items-center justify-between'>
-              <Label htmlFor='response_format'>JSON response:</Label>
-              <Switch
-                id='response_format'
-                name='response_format'
-                checked={settings.response_format}
-                onCheckedChange={() =>
-                  setSettings({
-                    ...settings,
-                    response_format: !settings.response_format,
-                  })
-                }
-              />
+            <div>
+              <div className='flex items-center justify-between'>
+                <Label htmlFor='response_format'>JSON response:</Label>
+                <Switch
+                  id='response_format'
+                  name='response_format'
+                  checked={settings.response_format}
+                  onCheckedChange={() =>
+                    setSettings({
+                      ...settings,
+                      response_format: !settings.response_format,
+                    })
+                  }
+                />
+              </div>
+
+              <p className='text-xs text-brand'>
+                An object specifying the format that the model must output.
+                Compatible with GPT-4 Turbo and all GPT-3.5 Turbo models newer
+                than gpt-3.5-turbo-1106. Setting to true enables JSON mode,
+                which guarantees the message the model generates is valid JSON.
+                Important: when using JSON mode, you must also instruct the
+                model to produce JSON yourself via a system or user message.
+                Without this, the model may generate an unending stream of
+                whitespace until the generation reaches the token limit,
+                resulting in a long-running and seemingly &quot;stuck&quot;
+                request.
+              </p>
             </div>
 
             <div className='flex items-center justify-between'>
@@ -206,12 +272,15 @@ export default function DefaultSettingsForm({ user }) {
         <TabsContent value='image'>
           <form
             action={updateDefaultImageSettingsAction}
-            className='flex flex-col gap-2'
+            className='flex flex-col gap-4'
           >
-            <div className='flex items-center justify-between gap-2'>
+            <div className='space-y-1'>
               <Label htmlFor='model' className='w-40'>
                 Model:
               </Label>
+              <p className='text-xs text-brand'>
+                The model to use for image generation.
+              </p>
               <Select
                 id='imageModel'
                 name='imageModel'
@@ -241,10 +310,14 @@ export default function DefaultSettingsForm({ user }) {
               </Select>
             </div>
 
-            <div className='flex items-center justify-between gap-2'>
+            <div className='space-y-1'>
               <Label htmlFor='n' className='w-40'>
                 Number of images:
               </Label>
+              <p className='text-xs text-brand'>
+                The number of images to generate. Must be between 1 and 10. For
+                dall-e-3, only n=1 is supported.
+              </p>
               <Select
                 id='n'
                 name='n'
@@ -268,10 +341,15 @@ export default function DefaultSettingsForm({ user }) {
               </Select>
             </div>
 
-            <div className='flex items-center justify-between gap-2'>
+            <div className='space-y-1'>
               <Label htmlFor='quality' className='w-40'>
                 Quality:
               </Label>
+              <p className='text-xs text-brand'>
+                The quality of the image that will be generated. hd creates
+                images with finer details and greater consistency across the
+                image. This param is only supported for dall-e-3.
+              </p>
               <Select
                 id='quality'
                 name='quality'
@@ -299,10 +377,15 @@ export default function DefaultSettingsForm({ user }) {
               </Select>
             </div>
 
-            <div className='flex items-center justify-between gap-2'>
+            <div className='space-y-1'>
               <Label htmlFor='size' className='w-40'>
                 Size:
               </Label>
+              <p className='text-xs text-brand'>
+                The size of the generated images. Must be one of 256x256,
+                512x512, or 1024x1024 for dall-e-2. Must be one of 1024x1024,
+                1792x1024, or 1024x1792 for dall-e-3 models.
+              </p>
               <Select
                 id='size'
                 name='size'
@@ -334,10 +417,17 @@ export default function DefaultSettingsForm({ user }) {
               </Select>
             </div>
 
-            <div className='flex items-center justify-between gap-2'>
+            <div className='space-y-1'>
               <Label htmlFor='style' className='w-40'>
                 Style:
               </Label>
+              <p className='text-xs text-brand'>
+                The style of the generated images. Must be one of vivid or
+                natural. Vivid causes the model to lean towards generating
+                hyper-real and dramatic images. Natural causes the model to
+                produce more natural, less hyper-real looking images. This param
+                is only supported for dall-e-3.
+              </p>
               <Select
                 id='style'
                 name='style'

a020bae2c1d41888fb6da3e5317d94f56d392831
Jon Green <jongreen1996@gmail.com>
2024-04-05 12:10:11 +0100

    Add credit estimates to productCard

--- src/components/ui/productCard.jsx
+++ src/components/ui/productCard.jsx
@@ -6,7 +6,7 @@ export default function ProductCard({ price }) {
   return (
     <label htmlFor={price}>
       <Card>
-        <CardHeader className='p-2'>
+        <CardHeader className='rounded-t-lg bg-muted p-2'>
           <CardTitle className='mx-auto text-xl'>
             <Input
               type='radio'
@@ -22,45 +22,99 @@ export default function ProductCard({ price }) {
             </p>
           </CardTitle>
         </CardHeader>
+
         <CardContent className='p-2'>
           <p>{price / 100} Million credits is equivalent to:</p>
+
           <ul className='list-disc pl-6'>
             <li>
               <p>
                 <strong>
-                  {Math.floor(
-                    (
-                      (price * 10_000) /
-                      1.3 /
+                  <TextCalculation price={price} model='gpt-3.5-turbo' />
+                </strong>{' '}
+                GPT-3.5 Turbo words
+              </p>
+            </li>
+
+            <li>
+              <p>
+                <strong>
+                  <TextCalculation price={price} model='gpt-4-turbo-preview' />
+                </strong>{' '}
+                GPT-4 Turbo words
+              </p>
+            </li>
+
+            <li>
+              <p>
+                <strong>
+                  <TextCalculation price={price} model='gpt-4' />
+                </strong>{' '}
+                GPT-4 words
+              </p>
+            </li>
+
+            <br />
+
+            <li>
+              <p>
+                <strong>
+                  <ImageCalculation
+                    price={price}
+                    model={
                       openAIModels['dall-e-2'].resTokens.standard['1024x1024']
-                    ).toLocaleString(),
-                  )}
+                    }
+                  />
                 </strong>{' '}
                 DALL-E-2 Images
               </p>
             </li>
+
             <li>
               <p>
                 <strong>
-                  {Math.floor(
-                    (
-                      (price * 10_000) /
-                      1.3 /
-                      openAIModels['dall-e-3'].resTokens.hd['1024x1024']
-                    ).toLocaleString(),
-                  )}
+                  <ImageCalculation
+                    price={price}
+                    model={
+                      openAIModels['dall-e-3'].resTokens.standard['1024x1024']
+                    }
+                  />
                 </strong>{' '}
                 DALL-E-3 Images
               </p>
             </li>
+
             <li>
               <p>
-                <strong></strong>
+                <strong>
+                  <ImageCalculation
+                    price={price}
+                    model={openAIModels['dall-e-3'].resTokens.hd['1024x1024']}
+                  />
+                </strong>{' '}
+                DALL-E-3 HD Images
               </p>
             </li>
           </ul>
+
+          <p className='text-sm text-muted-foreground'>
+            * Words based on 75% word to response token ratio
+          </p>
+          <p className='text-sm text-muted-foreground'>
+            * Image numbers based on 1024x1024 resolution
+          </p>
         </CardContent>
       </Card>
     </label>
   );
+}
+
+function ImageCalculation({ price, model }) {
+  return Math.floor((price * 10_000) / (model * 1.3)).toLocaleString();
+}
+
+function TextCalculation({ price, model }) {
+  return Math.floor(
+    ((price * 10_000) / (openAIModels[model].resTokens * 1.3)) * 0.75,
+  ).toLocaleString();
 }
